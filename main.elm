6c5f7d1956d612bdb9973a8d2ab11610f0e6fc4a
Pascal Le Merrer <pascal.lemerrer@orange.com>
2017-11-11 16:35:44 +0100

    Add annotation types

--- main.elm
+++ main.elm
@@ -3,15 +3,18 @@ module Main exposing (main)
 import Html exposing (..)
 
 
+initialModel : List String
 initialModel =
     [ "Luke", "Leia", "Han" ]
 
 
+view : List String -> Html msg
 view model =
     ul [] <|
         List.map viewItem model
 
 
+viewItem : String -> Html msg
 viewItem name =
     li [] [ text name ]
 
